fa08ca974a8c0948a46ae80d56e0e6c54634717b
Mary <89971822+Mary-fox@users.noreply.github.com>
2023-02-23 15:20:24 +0300

    feat: change link in playlist

--- momentum/js/playList.js
+++ momentum/js/playList.js
@@ -1,17 +1,17 @@
 const playList = [
     {
       title: "Linkin Park - Numb",
-      src: "../assets/sounds/Linkin Park Numb.mp3",
+      src: "assets/sounds/Linkin Park Numb.mp3",
       duration: "03:05",
     },
     {
       title: "Linkin Park - Lost",
-      src: "../assets/sounds/Linkin Park Lost.mp3",
+      src: "ssets/sounds/Linkin Park Lost.mp3",
       duration: "03:12",
     },
     {
       title: "River Flows In You",
-      src: "../assets/sounds/River Flows In You.mp3",
+      src: "assets/sounds/River Flows In You.mp3",
       duration: "01:37",
     },
   ];
