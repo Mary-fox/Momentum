6dbff955ea650c842b196538a9abd0c9b2184842
Mary <89971822+Mary-fox@users.noreply.github.com>
2023-02-25 13:57:34 +0300

    feat: add change in audio

--- momentum/js/audio.js
+++ momentum/js/audio.js
@@ -9,6 +9,21 @@ const btnNext = document.querySelector('.play-next');
 const btnPrev = document.querySelector('.play-prev');
 const playListAllSong = document.querySelector('.play-list');
 const audio = new Audio();
+const playName = document.querySelector('.play-name');
+const durationSound = document.querySelector('.play-duration');
+
+
+function playNameAndDuration() {
+    let restoreName = playName.textContent;
+    playName.textContent = playList[playNum].title;
+    playName.textContent = playList[playNum].title;
+    durationSound.textContent = playList[playNum].duration;
+    if (restoreName != playList[playNum].title) {
+        audio.currentTime = 0;
+    } else {
+        audio.currentTime = progressInput.value;
+    }
+}
 
 function selectionSong() {
     const songItems = document.querySelectorAll('.play-item');
@@ -25,11 +40,13 @@ function playOrStopAudio() {
     audio.currentTime = currentTime;
     audio.play();
     btnAudio.classList.toggle('pause');
-    selectionSong()
+    selectionSong();
+    playNameAndDuration();
     }else {
         audio.pause();
         isPlay = false;
-        btnAudio.classList.toggle('pause');}
+        btnAudio.classList.toggle('pause');
+        playNameAndDuration();}
 } 
     
 btnAudio.addEventListener('click', playOrStopAudio)
@@ -44,6 +61,7 @@ function playNext() {
     isPlay = true;
     btnAudio.classList.add('pause');  
     selectionSong()
+    playNameAndDuration();
   }
   
 function playPrev() {
@@ -51,10 +69,12 @@ function playPrev() {
     playNum = playNum - 1}
     audio.src = playList[playNum].src;
     audio.currentTime = currentTime;
+
     audio.play();
     isPlay = true;
     btnAudio.classList.add('pause');  
     selectionSong()
+    playNameAndDuration();
     }
 btnNext.addEventListener('click', playNext)
 btnPrev.addEventListener('click', playPrev)  
@@ -66,8 +86,68 @@ playList.forEach(e => {
     let itemSong =  Object.values(e);
     li.textContent = `${itemSong[0]}`;
     playListAllSong.append(li);
-    })
-  
-
-  
+    })  
 audio.addEventListener('ended', playNext);
+
+
+
+const progressInput = document.querySelector('.play-progress');
+const volumeProgress = document.querySelector('.volume-progress');
+const volume = document.querySelector('.play-volume');
+
+function changeProgressInput() {
+    audio.currentTime = progressInput.value;
+};
+progressInput.addEventListener('input', changeProgressInput);
+
+function offSoundOnInput() {
+    audio.volume = volumeProgress.value;
+    if (audio.volume === 0) {
+        volume.classList.add('off');
+    } else {
+        volume.classList.remove('off');
+    }
+}
+
+volumeProgress.addEventListener('input', offSoundOnInput);
+
+
+let restoreAudioValue;
+function offSound() {
+    if (volumeProgress.value != 0) {
+        restoreAudioValue = volumeProgress.value;
+        audio.volume = 0;
+        volumeProgress.value = 0;
+        volume.classList.add('off');
+    } else {
+        volume.classList.remove('off');
+        volumeProgress.value = restoreAudioValue;
+        audio.volume = volumeProgress.value;
+    }
+}
+
+volume.addEventListener('click', offSound);
+
+const currentNowTime = document.querySelector('.play-time');
+
+
+
+function formatTime(seconds) {
+    let min = Math.floor((seconds / 60));
+    let sec = Math.floor(seconds - (min * 60));
+    if (sec < 10){ 
+        sec  = `0${sec}`;
+    };
+    return `${min}:${sec}`;
+};
+function updateProgressValue() {
+    progressInput.max = audio.duration;
+    progressInput.value = audio.currentTime;
+    currentNowTime.innerHTML = (formatTime(Math.floor(audio.currentTime)));
+    setTimeout(updateProgressValue, 1000);
+};
+updateProgressValue();
+
+
+
+
