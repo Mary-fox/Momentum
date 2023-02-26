47c79dfd89d8cff71a0e91dff4c188ce6f7e5af1
Mary <89971822+Mary-fox@users.noreply.github.com>
2023-02-27 02:47:09 +0300

    feat: change getTimeOfDay

--- momentum/js/script.js
+++ momentum/js/script.js
@@ -36,9 +36,9 @@ function getTimeOfDay() {
     {return 'morning'} 
     if (hours > 11 && hours <18) 
     {return "afternoon";}
-    if (hours >= 17 && hours <24) 
+    if (hours >= 17 && hours <=24) 
     {return "evening";} 
-    if (hours >= 23 && hours <6) 
+    if (hours >=0  && hours <6) 
     {return "night";}
 }
 function getTimeOfDayLangRu() {
@@ -47,9 +47,9 @@ function getTimeOfDayLangRu() {
   {return 'Доброе утро,'} 
   if (hours > 11 && hours <18) 
   {return "Добрый день,";}
-  if (hours >= 17 && hours <24) 
+  if (hours >= 17 && hours <=24) 
   {return "Добрый вечер,";} 
-  if (hours >= 23 && hours <6) 
+  if (hours >= 0 && hours <6) 
   {return "Доброй ночи,";}
 }
 
